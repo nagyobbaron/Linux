dd8c2559d26e8fa76199166c13c4e1686d2a3f50
Nagy Aron <nagyobbaron@gmail.com>
2021-05-08 23:42:59 +0300

    Update configure_ethx.sh

--- Linux-setup/configure_ethx.sh
+++ Linux-setup/configure_ethx.sh
@@ -3,4 +3,4 @@
 sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
 update-grub
 
-sed -i "s/$(netstat -i | grep ens | cut -d" " -f 1)/eth0/g" /etc/netplan/00-installer-config.yaml
+# sed -i "s/$(netstat -i | grep ens | cut -d" " -f 1)/eth0/g" /etc/netplan/00-installer-config.yaml
