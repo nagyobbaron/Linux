27c61adaa367a0c8c4efd8107f81de618cbc25d0
Nagy Aron <nagyobbaron@gmail.com>
2021-05-08 23:37:22 +0300

    Update disable-networkd.sh

--- Linux-setup/disable-networkd.sh
+++ Linux-setup/disable-networkd.sh
@@ -5,6 +5,6 @@ apt-get install ifupdown
 ifdown --force eth0 eth1 eth2 lo && ifup -a
 systemctl unmask networking
 systemctl enable networking --now
-systemctl stop systemd-networkd.socket systemd-networkd.service
-systemctl disable systemd-networkd.socket systemd-networkd.service
-systemctl mask systemd-networkd.socket systemd-networkd.service
+systemctl stop systemd-networkd.socket systemd-networkd.service systemd-networkd-wait-online.service
+systemctl disable systemd-networkd.socket systemd-networkd.service systemd-networkd-wait-online.service
+systemctl mask systemd-networkd.socket systemd-networkd.service systemd-networkd-wait-online.service
